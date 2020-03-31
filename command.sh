72643d2d29fa49f371edcafc88e7d3f39727a56a
mandeepkhatry <mandeepkhatry1997@gmail.com>
2020-03-31 10:35:44 +0545

    minor changes for testing

--- command.sh
+++ command.sh
@@ -1,1 +1,1 @@
-./gateway/reflex -r '\.json$' -s -- sh -c "./gateway/krakend run -c ./gateway/config.json -p 8080" & ./main 
+./gateway/reflex -r '\.json$' -s -- sh -c "./gateway/krakend run -c ./gateway/config.json -p 8080" & ./main &
